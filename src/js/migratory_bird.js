dddfab0d17344df02302de3b2feca6acdd29c1dd
ryosuke_sato <r-sato@feedforce.jp>
2019-08-08 12:11:19 +0900

    Support esa editor v2 beta

--- src/js/migratory_bird.js
+++ src/js/migratory_bird.js
@@ -1,3 +1,7 @@
+const toArray = (nodes) => {
+  return [].map.call(nodes, node => node);
+}
+
 const arrayOfSlackSvgPath = [
   '<path d="M165.964 15.838c-3.89-11.975-16.752-18.528-28.725-14.636-11.975 3.89-18.528 16.752-14.636 28.725l58.947 181.365c4.048 11.187 16.132 17.473 27.732 14.135 12.1-3.483 19.475-16.334 15.614-28.217L165.964 15.838" fill="#DFA22F"/>',
   '<path d="M74.626 45.516C70.734 33.542 57.873 26.989 45.9 30.879 33.924 34.77 27.37 47.631 31.263 59.606l58.948 181.366c4.047 11.186 16.132 17.473 27.732 14.132 12.099-3.481 19.474-16.332 15.613-28.217L74.626 45.516" fill="#3CB187"/>',
@@ -34,8 +38,18 @@ const pbCopy = (text) => {
   return text;
 }
 
+const getEsaFormBodyTextForV1 = () => {
+  const textArea = document.getElementById('post_body_md');
+  return textArea && textArea.value;
+}
+
+const getEsaFormBodyTextForV2Beta = () => {
+  const preTags = document.querySelectorAll('#js_tab_form .CodeMirror-lines pre');
+  return toArray(preTags).reduce((memo, preTag) => memo += `${preTag.textContent}\n`, '');
+}
+
 const getEsaFormBodyText = () => {
-  return document.getElementById('post_body_md').value;
+  return getEsaFormBodyTextForV1() || getEsaFormBodyTextForV2Beta();
 }
 
 const processText = (text) => {
@@ -55,12 +69,22 @@ const eventListener = () => {
   pbCopy(morning_meating);
 }
 
+const getEsaFormBodyForV1 = () => {
+  return document.querySelector('#post_form > div.form-group.form-body');
+}
+
+const getEsaFormBodyForV2Beta = () => {
+  return document.querySelector('#js_tab_form .CodeMirror-lines');
+}
+
+const getEsaFormBody = () => {
+  return getEsaFormBodyForV1() || getEsaFormBodyForV2Beta();
+}
+
 (() => {
   const slackIconSvg = createSlackIconSvg();
   slackIconSvg.addEventListener('click', eventListener);
 
-  const esaFormBody = document.querySelector('#post_form > div.form-group.form-body');
-  if (esaFormBody !== null) {
-    esaFormBody.appendChild(slackIconSvg);
-  }
+  const esaFormBody = getEsaFormBody();
+  if (esaFormBody !== null) { esaFormBody.appendChild(slackIconSvg) }
 })();
